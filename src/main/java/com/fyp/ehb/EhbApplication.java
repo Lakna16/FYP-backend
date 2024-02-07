6146f5a7c9e4c210485b8d75a802e9f0cdb09446
DilmiN-1 <dilmini@lolctech.com>
2024-02-07 14:52:22 +0530

    initial commit

---
+++ src/main/java/com/fyp/ehb/EhbApplication.java
@@ -0,0 +1,13 @@
+package com.fyp.ehb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fyp/ehb/EhbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fyp.ehb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EhbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
