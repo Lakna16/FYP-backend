b5919bf9b6f4b89c6e41df15b27de3fa69419c58
DilmiN-1 <dilmini@lolctech.com>
2024-02-09 23:40:29 +0530

    classes added

---
+++ src/main/java/com/fyp/ehb/controller/BudgetController.java
@@ -0,0 +1,10 @@
+package com.fyp.ehb.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/budget")
+public class BudgetController {
+
+}
---
+++ src/main/java/com/fyp/ehb/service/CustomerServiceImpl.java
@@ -0,0 +1,8 @@
+package com.fyp.ehb.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+}
